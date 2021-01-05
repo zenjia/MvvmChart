854e129265a5fa5eec3bfbc5befd9d0b1fff1e75
zenjia <naivecoder@outlook.com>
2021-01-05 11:34:49 +0800

    remove nuget.config

--- WPF/WpfFX/MvvmChartWpfFX/Properties/AssemblyInfo.cs
+++ WPF/WpfFX/MvvmChartWpfFX/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
@@ -6,11 +7,11 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Cartesian2DChart")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MvvmChartWpfFX")]
+[assembly: AssemblyDescription("A lightweighted, mvvm support chart for WPF/UWP")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cartesian2DChart")]
+[assembly: AssemblyCompany("zenjia")]
+[assembly: AssemblyProduct("MvvmChartWpfFX")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -50,11 +51,12 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
 
 [assembly: XmlnsDefinition("http://schemas.mvvmchart.com/2020", "MvvmCharting.WpfFX", AssemblyName = "MvvmChartWpfFX")]
 [assembly: XmlnsDefinition("http://schemas.mvvmchart.com/2020", "MvvmCharting.WpfFX.Series", AssemblyName = "MvvmChartWpfFX")]
 [assembly: XmlnsDefinition("http://schemas.mvvmchart.com/2020", "MvvmCharting.WpfFX.Axis", AssemblyName = "MvvmChartWpfFX")]
 
-[assembly: XmlnsPrefix("http://schemas.mvvmchart.com/2020", "chart")] 
+[assembly: XmlnsPrefix("http://schemas.mvvmchart.com/2020", "chart")]
+[assembly: NeutralResourcesLanguage("en-US")]
